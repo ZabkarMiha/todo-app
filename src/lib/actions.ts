9810f831740a05e924e72f8b5a1bc65b2510fbbd
Miha Zabkar <miha.zabkar7@gmail.com>
2024-11-15 23:27:30 +0100

    Added task display get, delete and insert functions

---
+++ src/lib/actions.ts
@@ -0,0 +1,43 @@
+'use server'
+
+import {db} from "@/index";
+import {z} from "zod";
+import {taskFormSchema} from "@/lib/form-schemas";
+import {task} from "@/schema";
+import {revalidatePath} from "next/cache";
+import {desc, eq} from "drizzle-orm";
+
+export async function insertFormValues(values: z.infer<typeof taskFormSchema>){
+    try{
+        const data = await db.insert(task).values(values).returning({ returnedTitle: task.title, returnedDescription: task.description})
+        revalidatePath("/")
+        return data
+    }
+    catch(e){
+        console.log(e);
+        return null
+    }
+}
+
+export async function getTasks(){
+    try {
+        return await db.select().from(task).orderBy(desc(task.dateAdded))
+    }
+    catch(e){
+        console.log(e)
+        return null
+    }
+}
+
+export async function deleteTask(id: string){
+    try {
+        const data = await db.delete(task).where(eq(task.id, id)).returning({ returnedTitle: task.title, returnedDescription: task.description})
+        revalidatePath("/")
+        console.log(data)
+        return data
+    }
+    catch(e){
+        console.log(e);
+        return null
+    }
+}
