3c0e387369d1fd0a46fc3e17117375b640d3aeaa
Miha Zabkar <miha.zabkar7@gmail.com>
2024-11-16 22:53:17 +0100

    Updated task card look

--- src/components/todo-task-card.tsx
+++ src/components/todo-task-card.tsx
@@ -1,15 +1,44 @@
+'use client'
+
 import {Input} from "@/components/ui/input";
 import {Checkbox} from "@/components/ui/checkbox";
 import {Label} from "@/components/ui/label";
+import {Task} from "@/lib/types"
+import {Button} from "@/components/ui/button";
+import {deleteTask} from "@/lib/actions";
+import {toast} from "@/hooks/use-toast";
+import {useEffect, useState} from "react";
 
-export default function TodoTaskCard({title, description, completed}: Task) {
+export default function TodoTaskCard({task}: {task: Task}) {
+
+    const [date, setDate] = useState("")
+
+    useEffect(() => {
+        setDate(task.dateAdded.toLocaleDateString() + " " + task.dateAdded.toLocaleTimeString())
+    }, [task.dateAdded]);
+
+
+    const deleteTaskOnClick =  async (id: string) => {
+        const fetchedData = await deleteTask(id);
+
+        toast({
+            title: fetchedData === null ? "Uh oh! Something went wrong." : "Task deleted successfully",
+            description: fetchedData === null ? "There was a problem with your request." : JSON.stringify(fetchedData),
+        })
+    }
+
     return (
-        <div className="grid grid-rows-3 outline p-5">
-            <Label className={""}>{title}</Label>
-            <Label>{description}</Label>
+        <div className="grid grid-rows-3 h-56 outline p-5">
+            <Label className={""}>{task.title}</Label>
+            <Label>{task.description}</Label>
             <div>
                 <Label htmlFor={"completed"}>Completed</Label>
-                <Checkbox id={"completed"} checked={completed} />
+                <Checkbox id={"completed"} checked={task.completed} />
+            </div>
+            <Label className={""}>Date added: {date}</Label>
+            <div>
+                <Button variant={"destructive"} onClick={async () => deleteTaskOnClick(task.id)}>Delete</Button>
+                <Button variant={"outline"}>Edit</Button>
             </div>
         </div>
     );
