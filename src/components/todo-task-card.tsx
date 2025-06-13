f78da29e6fec9563dcb0d074ae16041fd039d2a5
Miha Zabkar <miha.zabkar7@gmail.com>
2025-06-13 03:55:30 +0200

    Remove useEffect for date in task, replace with toLocaleDateString with dd/mm/yyyy format

--- src/components/todo-task-card.tsx
+++ src/components/todo-task-card.tsx
@@ -1,37 +1,26 @@
-"use client"
-
 import { Toggle } from "@/components/ui/toggle"
 import { Label } from "@/components/ui/label"
 import { Task } from "@/lib/types"
 import { Button } from "@/components/ui/button"
 import { completeTaskToggle, deleteTask } from "@/lib/actions"
 import { toast } from "@/hooks/use-toast"
-import { useEffect, useState } from "react"
 import { CheckIcon } from "@radix-ui/react-icons"
 
 export default function TodoTaskCard({ task }: { task: Task }) {
-  const [date, setDate] = useState("")
-
-  useEffect(() => {
-    setDate(
-      task.dateAdded.toLocaleDateString() +
-        " " +
-        task.dateAdded.toLocaleTimeString()
-    )
-  }, [task.dateAdded])
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  } as const
 
   const deleteTaskOnClick = async () => {
     const result = await deleteTask(task.id)
 
     toast({
-      title:
-        result.error
-          ? "Uh oh! Something went wrong."
-          : "Task deleted successfully",
-      description:
-        result.error
-          ? result.error
-          : JSON.stringify(result.data),
+      title: result.error
+        ? "Uh oh! Something went wrong."
+        : "Task deleted successfully",
+      description: result.error ? result.error : JSON.stringify(result.data),
     })
   }
 
@@ -42,11 +31,7 @@ export default function TodoTaskCard({ task }: { task: Task }) {
   return (
     <div className="flex flex-col w-full outline outline-1 outline-neutral-200 dark:outline-neutral-800 rounded-md p-5 bg-neutral-50 dark:bg-neutral-900">
       <div className="flex flex-row items-center gap-2 mb-2">
-        <h1
-          className={
-            "text-4xl font-bold text-ellipsis overflow-hidden"
-          }
-        >
+        <h1 className={"text-4xl font-bold text-ellipsis overflow-hidden"}>
           {task.title}
         </h1>
         <Toggle
@@ -58,12 +43,16 @@ export default function TodoTaskCard({ task }: { task: Task }) {
           <CheckIcon />
         </Toggle>
       </div>
-      <p className="break-all hover:overflow-y-auto overflow-hidden mb-5">{task.description}</p>
+      <p className="break-all hover:overflow-y-auto overflow-hidden mb-5">
+        {task.description}
+      </p>
       <div className="flex flex-row items-center gap-2 self-center mt-auto w-full">
         <Button variant={"destructive"} onClick={deleteTaskOnClick}>
           Delete
         </Button>
-        <Label className={"text-center grow"}>{date}</Label>
+        <Label className={"text-center grow"}>
+          {task.dateAdded.toLocaleDateString("en-GB", options)}
+        </Label>
         <Button
           className={"hover:bg-neutral-300 dark:hover:bg-neutral-800"}
           variant={"outline"}
