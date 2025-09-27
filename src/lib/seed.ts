bfafbf4a8e8c6990e27b9d7c89070721097692f1
Miha Zabkar <miha.zabkar7@gmail.com>
2025-09-27 04:35:42 +0200

    Add arguments to seed script

--- src/lib/seed.ts
+++ src/lib/seed.ts
@@ -2,14 +2,16 @@ import { task } from "@/schema"
 import { db } from "@/index"
 
 async function Seed() {
-  for (let i = 1; i <= 5; i++) {
-    await db
-      .insert(task)
-      .values({
-        title: `title${i}`,
-        description: `description${i}`,
-        completed: false,
-      })
+  const args = process.argv.slice(2)
+  const numberArg = args.find((arg) => arg.startsWith("tasksCount="))
+  const tasksCount: number = numberArg ? Number(numberArg.split("=")[1]) : 5
+
+  for (let i = 1; i <= tasksCount; i++) {
+    await db.insert(task).values({
+      title: `title${i}`,
+      description: `description${i}`,
+      completed: false,
+    })
   }
 }
 
