b6be99acaa23c58c4a0991005a9054a84efebb0b
Miha Zabkar <miha.zabkar7@gmail.com>
2025-11-07 04:14:16 +0100

    Redesign dock mobile style

--- src/components/dock.tsx
+++ src/components/dock.tsx
@@ -15,19 +15,23 @@ export default function Dock({
   currentPage,
 }: DockProps) {
   return (
-    <div className="sticky bottom-2 outline outline-1 outline-container-outline backdrop-blur-xl p-2 rounded-md flex flex-col space-y-1 mx-auto md:flex-row md:self-center md:justify-center md:space-y-0 md:space-x-1">
-      <div className="flex flex-row space-x-5 p-1 items-center h-auto">
-        {filteredTasksLength > tasksPerPage && (
-          <PaginationBar
-            tasksCount={filteredTasksLength}
-            currentPage={currentPage}
-            tasksPerPage={tasksPerPage}
-            className="self-center h-full"
-          />
-        )}
-        <EditAddTask />
-        <Search className="h-full w-full" />
-        <SortTasks className="h-full" />
+    <div className="sticky bottom-4 h-fit outline outline-1 outline-container-outline backdrop-blur-xl p-2 rounded-md md:self-center">
+      <div className="flex flex-col space-x-2 space-y-2 p-1 md:flex-row md:space-y-0">
+        <div className="flex flex-row space-x-2 self-center">
+          {filteredTasksLength > tasksPerPage && (
+            <PaginationBar
+              tasksCount={filteredTasksLength}
+              currentPage={currentPage}
+              tasksPerPage={tasksPerPage}
+              className="self-center h-full"
+            />
+          )}
+          <EditAddTask />
+        </div>
+        <div className="flex flex-row space-x-2">
+          <Search className="h-full w-full" />
+          <SortTasks className="h-full" />
+        </div>
       </div>
     </div>
   )
