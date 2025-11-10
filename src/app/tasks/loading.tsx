850bf3d0691bf076a9cf8dcf7bf4cdaaf7434121
Miha Zabkar <miha.zabkar7@gmail.com>
2025-11-10 20:10:44 +0100

    Replace own implementation with Shadcn Card component, make loading responsive for mobile, add tailwind prettier plugin

--- src/app/tasks/loading.tsx
+++ src/app/tasks/loading.tsx
@@ -2,29 +2,29 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 export default function Loading() {
   return (
-    <div className="flex flex-col h-full mx-5 mb-32 md:mx-10 xl:mx-20">
+    <div className="flex flex-col mx-5 mb-2 mt-2 md:mx-10 xl:mx-20">
       <div className="hidden gap-5 2xl:grid 2xl:grid-cols-4 2xl:mx-0">
         {Array.from({ length: 8 }).map((_, index) => (
-          <Skeleton key={index} className="w-full rounded-md h-60" />
+          <Skeleton key={index} className="w-full rounded-md h-[40dvh]" />
         ))}
       </div>
       <div className="hidden gap-5 xl:grid 2xl:hidden xl:grid-cols-3 xl:mx-0">
         {Array.from({ length: 6 }).map((_, index) => (
-          <Skeleton key={index} className="w-full rounded-md h-60" />
+          <Skeleton key={index} className="w-full rounded-md h-[40dvh]" />
         ))}
       </div>
       <div className="hidden gap-5 md:grid xl:hidden md:grid-cols-2">
         {Array.from({ length: 4 }).map((_, index) => (
-          <Skeleton key={index} className="w-full rounded-md h-60" />
+          <Skeleton key={index} className="w-full rounded-md h-[40dvh]" />
         ))}
       </div>
       <div className="grid gap-5 md:hidden grid-cols-1">
         {Array.from({ length: 2 }).map((_, index) => (
-          <Skeleton key={index} className="w-full rounded-md h-60" />
+          <Skeleton key={index} className="w-full rounded-md h-[40dvh]" />
         ))}
       </div>
-      <div className="fixed left-0 right-0 bottom-2 z-50 w-[calc(100svw-1rem)] p-2 flex flex-col space-y-1 mx-auto md:flex-row md:self-center md:justify-center md:space-y-0 md:space-x-1 xl:w-fit xl:bottom-10">
-        <Skeleton className="w-80 h-12" />
+      <div className="sticky bottom-4 h-fit backdrop-blur-xl p-2 mx-2 rounded-md md:mx-0 md:self-center">
+        <Skeleton className="w-full h-24 md:h-12 md:w-lg" />
       </div>
     </div>
   )
--- src/components/todo-task-card.tsx
+++ src/components/todo-task-card.tsx
@@ -1,64 +1,75 @@
-import { Toggle } from "@/components/ui/toggle"
-import { Label } from "@/components/ui/label"
-import { ErrorData, Task } from "@/lib/types"
-import { Button } from "@/components/ui/button"
-import { completeTaskToggle, deleteTask } from "@/lib/actions/database"
-import { toast } from "sonner"
-import { CheckIcon, TrashIcon } from "@radix-ui/react-icons"
-import EditAddTask from "./edit-add-task"
-import { Separator } from "./ui/separator"
+import { Toggle } from "@/components/ui/toggle";
+import { Label } from "@/components/ui/label";
+import { ErrorData, Task } from "@/lib/types";
+import { Button } from "@/components/ui/button";
+import { completeTaskToggle, deleteTask } from "@/lib/actions/database";
+import { toast } from "sonner";
+import { CheckIcon, TrashIcon } from "@radix-ui/react-icons";
+import EditAddTask from "./edit-add-task";
+import { Separator } from "./ui/separator";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function TodoTaskCard({ task }: { task: Task }) {
   const options = {
     year: "numeric",
     month: "long",
     day: "numeric",
-  } as const
+  } as const;
 
   const deleteTaskOnClick = async () => {
     toast.promise(deleteTask(task.id), {
       loading: "Loading...",
       success: (data) => `${data.data?.title} has been deleted`,
       error: (error: ErrorData) => `${error.message}, ${error.status}`,
-    })
-  }
+    });
+  };
 
   const completedToggleOnChange = async (completed: boolean) => {
-    await completeTaskToggle(task.id, completed)
-  }
+    await completeTaskToggle(task.id, completed);
+  };
 
   return (
-    <div className="flex flex-col w-full outline-solid outline-1 outline-task-outline rounded-md p-5 bg-task-background space-y-4 max-h-[500px]">
-      <div className="flex flex-row items-center gap-2">
-        <span
+    <Card className="max-h-[500px]">
+      <CardHeader className="grid-rows-1">
+        <CardTitle
           className={
-            "text-2xl font-bold text-ellipsis overflow-hidden whitespace-nowrap xl:text-3xl"
+            "overflow-hidden text-2xl font-bold text-ellipsis whitespace-nowrap hover:whitespace-break-spaces hover:overflow-visible xl:text-3xl"
           }
         >
           {task.title}
-        </span>
-        <Toggle
-          className="ml-auto"
-          variant={"outline"}
-          pressed={task.completed}
-          onPressedChange={completedToggleOnChange}
-        >
-          <CheckIcon />
-        </Toggle>
-      </div>
-      <p className="h-full overflow-hidden text-pretty overflow-y-auto mb-5">
-        {task.description}
-      </p>
-      <Separator className="w-full" />
-      <div className="flex flex-row items-center gap-2 self-center mt-auto w-full">
+        </CardTitle>
+        <CardAction>
+          <Toggle
+            className="ml-auto"
+            variant={"outline"}
+            pressed={task.completed}
+            onPressedChange={completedToggleOnChange}
+          >
+            <CheckIcon />
+          </Toggle>
+        </CardAction>
+      </CardHeader>
+      <CardContent>
+        <CardDescription>{task.description}</CardDescription>
+      </CardContent>
+      <Separator className="mt-auto w-full" />
+      <CardFooter className="gap-2">
         <Button variant={"destructive"} onClick={deleteTaskOnClick}>
           <TrashIcon className="h-4 w-4" />
         </Button>
-        <Label className={"text-center grow"}>
+        <Label className={"mx-auto text-center"}>
           {task.dateAdded.toLocaleDateString("en-GB", options)}
         </Label>
         <EditAddTask taskData={task} />
-      </div>
-    </div>
-  )
+      </CardFooter>
+    </Card>
+  );
 }
