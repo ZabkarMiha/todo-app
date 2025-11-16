134f11407a90cf1004866c9e475b9e775f85ad5d
Miha Zabkar <miha.zabkar7@gmail.com>
2025-11-16 17:26:54 +0100

    Create image upload action with aws/s3, upload image component, use react-dropzone shadcn community component

---
+++ src/lib/s3/bucket.ts
@@ -0,0 +1,48 @@
+"use server";
+
+import {
+  PutObjectCommand,
+  PutObjectCommandInput,
+  PutObjectCommandOutput,
+  S3Client,
+  S3ServiceException,
+} from "@aws-sdk/client-s3";
+import { ActionResponse } from "../types";
+
+const s3client = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY as string,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+  },
+});
+
+export async function uploadImageToS3(
+  file: File,
+  username: string,
+): Promise<ActionResponse<PutObjectCommandOutput>> {
+  try {
+    const fileBuffer = Buffer.from(await file.bytes());
+    const extension = file.name.split(".")[1];
+    const finalKey = username + "-avatar." + extension;
+
+    const input: PutObjectCommandInput = {
+      Body: fileBuffer,
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: finalKey,
+    };
+    const command = new PutObjectCommand(input);
+    const response = await s3client.send(command);
+
+    return { data: response };
+  } catch (e) {
+    if (e instanceof S3ServiceException) {
+      return {
+        error: { message: e.message, status: e.$response?.statusCode! },
+      };
+    }
+    return {
+      error: { message: "Failed to upload due to unknown error", status: 500 },
+    };
+  }
+}
